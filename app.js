0d9f90178dd8c52b045625fb8962209e21d12593
Malik Shahzad <m.shahzad.mail.56@gmail.com>
2024-02-04 07:14:03 +0500

    m

---
+++ app.js
@@ -0,0 +1,8 @@
+function genrateOTP(length){
+    let otp = '';
+    for(let i = 0; i <length; i++){
+        otp +=Math.floor(Math.random() * 10).toString();
+    }
+    return otp
+}
+console.log(genrateOTP(6))
